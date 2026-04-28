75cacbe701fb454be737150fe4f6b50c6fa2c422
Gino Freud D. Hobayan <117270964+Gino-Freud-Hobayan@users.noreply.github.com>
2026-04-28 11:48:46 +0800

    Update Cplusplus-notes.cpp

--- Cplusplus-notes.cpp
+++ Cplusplus-notes.cpp
@@ -28,3 +28,45 @@ cout<<"\a";
 //1st alarm
 cout<<"\a"; //2nd alarm
 }
+
+
+
+
+
+
+
+
+
+
+
+// Example #2 : Functions that hasreturn value but doesn’t have argument
+
+#include<iostream>
+using namespace std;
+int square(void); 
+
+
+int main()
+{
+
+cout<<"***SQUARE OF A NUMBER***"; square();
+cout<<"\n***TRY ANOTHER ONE***"; square();
+
+}
+
+
+
+int square(void)  // Does not have an argument - because it is void
+{
+
+int a, sq;
+cout<<"\nEnter a number: "; cin>>a;
+sq = a * a;
+cout<<"\nThe square of "<<a<<" is "<<sq;
+
+}
+
+
+
+
+
